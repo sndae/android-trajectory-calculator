4957c2d6da0f62d919153a0355590ca0d3f1f5b6
fpinzn@gmail.com <fpinzn@gmail.com@c9df0a70-6049-ccb6-ef6f-87e30d84ee0b>
2011-02-23 22:27:12 +0000

    Plotting the raw information from the accelerometer and gyroscope. Excellent results without using moving average.

--- trunk/tests/PathDrawer2D/src/me/pacho/android/PathDrawer2D/Panel.java
+++ trunk/tests/PathDrawer2D/src/me/pacho/android/PathDrawer2D/Panel.java
@@ -13,6 +13,7 @@ class Panel extends SurfaceView implements SurfaceHolder.Callback {
 	private SensorGuy sensorGuy;
 	private int count=0;
 	private int[] screenSize=new int[2]; 
+	
 	public Panel(Context context, AttributeSet attrs) {
           super(context, attrs);
       getHolder().addCallback(this);
@@ -53,33 +54,61 @@ class Panel extends SurfaceView implements SurfaceHolder.Callback {
     
     @Override
     public void onDraw(Canvas canvas) {
-        Paint paint = new Paint();
-        paint.setColor(Color.BLACK);
-
-    	if(count==0){
-    		canvas.drawRect(0,0,screenSize[0],screenSize[1],paint);
-
-    	}
-
         screenSize[0]=canvas.getWidth();
         screenSize[1]=canvas.getHeight();
-        //entre 5 y -5 m/s^2
-        float[] acceleration=sensorGuy.getNetAcceleration();//{3f,0f,-3f};//
-        float scale=screenSize[1]/8;
-        int zero=(int)(screenSize[1]*0.4);
-      //x
-        paint.setColor(Color.RED);
-        canvas.drawCircle(count, acceleration[0]*scale+zero,2f, paint);
-      //y
-        paint.setColor(Color.BLUE);
-        canvas.drawCircle(count, acceleration[1]*scale+zero,2f, paint);
-      //z
-        paint.setColor(Color.WHITE);
-        canvas.drawCircle(count, acceleration[2]*scale+zero,2f, paint);
         
-        canvas.drawRect(0,0,screenSize[0],50, paint);
-        paint.setColor(Color.BLACK);
-        canvas.drawText("Count: "+count, 5,20, paint);
+        float[] acceleration=sensorGuy.getAcceleration();        
+        float scale_accel=screenSize[1]/45;
+        int zero_accel=(int)(screenSize[1]*0.25);
+        
+        
+        float[] orientation=sensorGuy.getGyroscope();
+        float scale_orientation=screenSize[1]/720;
+        int zero_orientation=(int)(screenSize[1]*0.75);
+        
+        //BG Operations
+	        Paint paint = new Paint();
+	        paint.setColor(Color.BLACK);
+	
+	    	if(count==0){
+	    		canvas.drawRect(0,0,screenSize[0],screenSize[1],paint);
+	
+	    	}
+	    	paint.setColor(Color.WHITE);
+	    	canvas.drawLine(0, screenSize[1]/2, screenSize[0], screenSize[1]/2, paint);
+	        canvas.drawText("Accelerometer(m/s^2)", 5,screenSize[1]/2-5, paint);
+	        canvas.drawText("Gyroscope(deg/s)",screenSize[0]-90 ,screenSize[1]/2+15, paint);
+	
+	    	canvas.drawLine(0, zero_accel, screenSize[0], zero_accel, paint);
+	
+	        canvas.drawRect(0,0,screenSize[0],25, paint);
+	        paint.setColor(Color.BLACK);
+	        canvas.drawText("Count: "+count, 5,20, paint);
+       
+      //Accelerometer  
+	      //x
+	        paint.setColor(Color.RED);
+	        canvas.drawCircle(count, acceleration[0]*scale_accel+zero_accel,2f, paint);
+	      //y
+	        paint.setColor(Color.BLUE);
+	        canvas.drawCircle(count, acceleration[1]*scale_accel+zero_accel,2f, paint);
+	      //z
+	        paint.setColor(Color.GREEN);
+	        canvas.drawCircle(count, acceleration[2]*scale_accel+zero_accel,2f, paint);
+	        
+	 //Orientation
+		  //x
+	        paint.setColor(Color.MAGENTA);
+	        canvas.drawCircle(count, -(orientation[0]-180)*scale_orientation+zero_orientation,2f, paint);
+	      //y
+	        paint.setColor(Color.CYAN);
+	        canvas.drawCircle(count, -(orientation[1]-180)*scale_orientation+zero_orientation,2f, paint);
+	      //z
+	        paint.setColor(Color.YELLOW);
+	        canvas.drawCircle(count,- (orientation[2]-180)*scale_orientation+zero_orientation,2f, paint);
+         
+        
+        
         count++;
         if(count>screenSize[0]){
         	count=0;
