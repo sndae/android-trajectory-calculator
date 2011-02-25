3040a2cfd2178c69411d177851d7b448dd051404
fpinzn@gmail.com <fpinzn@gmail.com@c9df0a70-6049-ccb6-ef6f-87e30d84ee0b>
2011-02-25 17:17:31 +0000

    Fixed the net acceleration problem, at last it's able to calculate de net acceleration (without gravity), to calculate the orientation, and the angular speed without noise.

--- trunk/tests/PathDrawer2D/src/me/pacho/android/PathDrawer2D/Drawer.java
+++ trunk/tests/PathDrawer2D/src/me/pacho/android/PathDrawer2D/Drawer.java
@@ -3,10 +3,13 @@ package me.pacho.android.PathDrawer2D;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.view.KeyEvent;
 
 public class Drawer extends Activity {
 	private static Context CONTEXT;
 	private static SensorGuy sensorGuy;
+	private static RawData rawData;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -27,5 +30,30 @@ public class Drawer extends Activity {
     
     public static Context getContext() {
 		return CONTEXT;
+	}
+    
+    static void registerRawData(RawData rd){
+    	 rawData = rd;
+    }
+    
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event){
+		//4 back
+		//82 menu
+		//84 buscar
+		//25 low vol
+		//24 + vol
+		if(rawData!=null){
+		if(keyCode==KeyEvent.KEYCODE_MENU){
+			//TODO
+		}
+		else if(keyCode==KeyEvent.KEYCODE_BACK){
+			rawData.reset();
+		}
+		else if(keyCode==KeyEvent.KEYCODE_SEARCH){
+			
+		}
+		}
+		return true;
 	}
 }
--- trunk/tests/PathDrawer2D/src/me/pacho/android/PathDrawer2D/SensorGuy.java
+++ trunk/tests/PathDrawer2D/src/me/pacho/android/PathDrawer2D/SensorGuy.java
@@ -1,6 +1,5 @@
 package me.pacho.android.PathDrawer2D;
 
-import java.util.ArrayList;
 import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -9,19 +8,26 @@ import android.hardware.SensorManager;
 import android.util.Log;
 
 
+
 public class SensorGuy implements SensorEventListener{
 
     private Sensor mAccelerometer;
     private Sensor mGyroscope;
     private Sensor mGravity;
+    private Sensor	mMagnetic;
 	private SensorManager mSensorManager;
 
+    
+    private float[] lastGravity={0f,0f,0f};
+    private float[] lastGyroscope={0f,0f,0f};
+    private float[] lastAcceleration={0f,0f,0f};
+    private float[] lastMagnetic={0f,0f,0f};
+    
+    
+    private float[] mOrientation = new float[3]; 
+    private float[] mRotationM = new float[9];               // Use [16] to co-operate with android.opengl.Matrix 
+    private float[] mRemapedRotationM = new float[9]; 
 
-    
-    private float[] lastGravity=new float[3];
-    private float[] lastGyroscope=new float[3];
-    private float[] lastAcceleration=new float[3];
-    
     public SensorGuy(){
     	Drawer.getContext();
     	if(Drawer.getContext()==null){
@@ -37,7 +43,11 @@ public class SensorGuy implements SensorEventListener{
         
         mGravity=mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
         mSensorManager.registerListener(this, mGravity, SensorManager.SENSOR_DELAY_FASTEST);
-
+        
+        mMagnetic=mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        mSensorManager.registerListener(this, mMagnetic, SensorManager.SENSOR_DELAY_FASTEST);
+        
+        
     }
 	@Override
 	public void onAccuracyChanged(Sensor arg0, int arg1) {
@@ -49,37 +59,56 @@ public class SensorGuy implements SensorEventListener{
 	 public void onSensorChanged(SensorEvent event) {
         if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
         	copyArray(lastAcceleration,event.values);
-       	
+        	calculateOrientation();
+
         }
         else if(event.sensor.getType() ==Sensor.TYPE_GYROSCOPE){
         	copyArray(lastGyroscope,event.values);
-
-
         }
         else if (event.sensor.getType()== Sensor.TYPE_GRAVITY){
         	copyArray(lastGravity,event.values);
         }
+        else if (event.sensor.getType()== Sensor.TYPE_MAGNETIC_FIELD){
+        	copyArray(lastMagnetic,event.values);
+        	calculateOrientation();
+        }
      
         else return;
 		
+
+
     }
+	
 
+	private void calculateOrientation(){
+        if(SensorManager.getRotationMatrix(mRotationM, null, lastAcceleration, lastMagnetic)){
+        	SensorManager.remapCoordinateSystem(mRotationM, SensorManager.AXIS_X, SensorManager.AXIS_Z, mRemapedRotationM); 
+            SensorManager.getOrientation(mRemapedRotationM, mOrientation); 
 
+        }
+	}
 	
 	private void copyArray(float[] destination, float[] source) {
 		System.arraycopy(source, 0, destination, 0, 3);
 	}
 	public float[] getAcceleration(){
-		return lastAcceleration;
+		float[] resp={lastAcceleration[0]-lastGravity[0],lastAcceleration[1]-lastGravity[1],lastAcceleration[2]-lastGravity[2]};
+		return resp;
 	}	
 
 	public float[] getGravity(){
 		return lastGravity;
 	}
-	public float[] getGyroscope(){
-		return new float[]{((Math.round((Math.toDegrees(lastGyroscope[0])) *2)/2)+360)%360,
-							((Math.round((Math.toDegrees(lastGyroscope[1])) *2)/2)+360)%360,
-							((Math.round((Math.toDegrees(lastGyroscope[2])) *2)/2)+360)%360};
+	public float[] getGyroscope(){	
+		return new float[]{rad2deg(lastGyroscope[0]),rad2deg(lastGyroscope[1]),rad2deg(lastGyroscope[2])};
 	}
+	public float[] getOrientation(){
+		return new float[]{rad2deg(mOrientation[0]),rad2deg(mOrientation[1]),rad2deg(mOrientation[2])};
+	}
+	
+	public float rad2deg(float value){
+		return ((Math.round((Math.toDegrees(value)) *2)/2)+360)%360;
+	}
+
 
 }
