50623f25c1728e558d58d674965c30f940aa7ae2
fpinzn <fpinzn@gmail.com>
2011-02-28 16:07:26 +0000

    Draws and calculates 2D Path, still has a telescopic error in the calculation.

--- tests/PathDrawer2D/src/me/pacho/android/PathDrawer2D/PathDrawer.java
+++ tests/PathDrawer2D/src/me/pacho/android/PathDrawer2D/PathDrawer.java
@@ -8,13 +8,13 @@ import android.util.AttributeSet;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
-class RawData extends SurfaceView implements SurfaceHolder.Callback {
+class PathDrawer extends SurfaceView implements SurfaceHolder.Callback {
 	private CanvasThread canvasthread;
-	private SensorGuy sensorGuy;
+	private PathManager pathManager;
 	private int count=0;
 	private int[] screenSize=new int[2]; 
 	
-	public RawData(Context context, AttributeSet attrs) {
+	public PathDrawer(Context context, AttributeSet attrs) {
       super(context, attrs);
       getHolder().addCallback(this);
       canvasthread = new CanvasThread(getHolder(), this);
@@ -32,7 +32,7 @@ class RawData extends SurfaceView implements SurfaceHolder.Callback {
             // TODO Auto-generated method stub
         canvasthread.setRunning(true);
         canvasthread.start();
-        sensorGuy=Drawer.getSensorGuy();
+        pathManager=Drawer.getPathManager();
         Drawer.registerRawData(this);
     }
     @Override
@@ -56,33 +56,57 @@ class RawData extends SurfaceView implements SurfaceHolder.Callback {
         screenSize[0]=canvas.getWidth();
         screenSize[1]=canvas.getHeight();
         
-        
+        Float[] position=pathManager.getLastPosition();
+        float[] nacc=pathManager.lastNetAcceleration;
+        float[] vel=pathManager.lastLinealVelocity;
+        float[] ori=pathManager.lastOrientation;
         /*float[] acceleration=sensorGuy.getAcceleration();        
         float scale_accel=screenSize[1]/45;
         int zero_accel=(int)(screenSize[1]*0.25);*/
         
-        float[] gyroscope=sensorGuy.getGyroscope();        
+       /* float[] gyroscope=sensorGuy.getGyroscope();        
         int zero_gyro=(int)(screenSize[1]*0.25);
         
         
         float[] orientation=sensorGuy.getOrientation();
         float scale_orientation=screenSize[1]/720;
-        int zero_orientation=(int)(screenSize[1]*0.75);
+        int zero_orientation=(int)(screenSize[1]*0.75);*/
         
         //BG Operations
 	        Paint paint = new Paint();
-	        paint.setColor(Color.BLACK);
-	
-	    	if(count==0){
-	    		canvas.drawColor(Color.BLACK);
-	    	}
-	        
+
 	    	paint.setColor(Color.WHITE);
 	    	canvas.drawLine(0, screenSize[1]/2, screenSize[0], screenSize[1]/2, paint);  
 
-	        canvas.drawRect(0,0,screenSize[0],25, paint);
+	        canvas.drawRect(0,0,screenSize[0],100, paint);
 	        paint.setColor(Color.BLACK);
-	        canvas.drawText("Count: "+count, 5,20, paint);
+	        canvas.drawText("Pos: "+ String.format("%.2f", position[0])+","+String.format("%.2f", position[1])+","+ String.format("%.2f", position[2]), 5,20, paint);
+	        canvas.drawText("NACC: "+ String.format("%.2f", nacc[0])+","+String.format("%.2f", nacc[1])+","+nacc[2], 5,40, paint);
+	        canvas.drawText("VEL: "+ String.format("%.2f", vel[0])+","+String.format("%.2f", vel[1])+","+vel[2], 5,60, paint);
+	        canvas.drawText("ORI: "+ String.format("%.2f", ori[0])+","+String.format("%.2f", ori[1])+","+ori[2], 5,80, paint);
+	       /* canvas.drawText("NDT: "+pathManager.nanoDeltaTime, 5,140, paint);
+	        canvas.drawText("TS: "+pathManager.lastTimeStamp+","+pathManager.deltaTime, 5,170, paint);*/
+	        
+
+	        float scale_accel=screenSize[1]/90;
+	        int zero_accel=(int)(screenSize[1]*0.5);
+	        
+	           canvas.drawLine(0, screenSize[1]/2, screenSize[0], screenSize[1]/2, paint);  
+	      	   canvas.drawText("Pos(m)", 5,screenSize[1]/2-5, paint);
+
+		      //x
+		        paint.setColor(Color.RED);
+		        canvas.drawCircle(position[0]*scale_accel+zero_accel, position[1]*scale_accel+zero_accel,2f, paint);
+		      /*y
+		        paint.setColor(Color.BLUE);
+		        canvas.drawCircle(count, position[1]*scale_accel+zero_accel,2f, paint);
+		      //z
+		        paint.setColor(Color.GREEN);
+		        canvas.drawCircle(count, position[2]*scale_accel+zero_accel,2f, paint);*/
+	        
+	        
+	        
+	        
      /*  
       //Accelerometer 
            paint.setColor(Color.WHITE);
@@ -102,7 +126,7 @@ class RawData extends SurfaceView implements SurfaceHolder.Callback {
 	        canvas.drawCircle(count, acceleration[2]*scale_accel+zero_accel,2f, paint);
 	  */   
 	  //Gyroscope
-	        paint.setColor(Color.WHITE);
+	  /*      paint.setColor(Color.WHITE);
 	      	canvas.drawText("Gyroscope(deg/seg)", 5,screenSize[1]/2-5, paint);
 
 	      //x
@@ -113,10 +137,9 @@ class RawData extends SurfaceView implements SurfaceHolder.Callback {
 	        canvas.drawCircle(count,-(gyroscope[1]-180)*scale_orientation+zero_gyro,2f, paint);
 	      //z
 	        paint.setColor(Color.GREEN);
-	        canvas.drawCircle(count, -(gyroscope[2]-180)*scale_orientation+zero_gyro,2f, paint);
-	       
+	        canvas.drawCircle(count, -(gyroscope[2]-180)*scale_orientation+zero_gyro,2f, paint);*/
 	 //Orientation
-	        paint.setColor(Color.WHITE);
+	 /*       paint.setColor(Color.WHITE);
 	    	canvas.drawText("Orientation(deg)",screenSize[0]-90 ,screenSize[1]/2+15, paint);
 
 		  //x
@@ -127,7 +150,7 @@ class RawData extends SurfaceView implements SurfaceHolder.Callback {
 	        canvas.drawCircle(count, -(orientation[1]-180)*scale_orientation+zero_orientation,2f, paint);
 	      //z
 	        paint.setColor(Color.YELLOW);
-	        canvas.drawCircle(count,- (orientation[2]-180)*scale_orientation+zero_orientation,2f, paint);
+	        canvas.drawCircle(count,- (orientation[2]-180)*scale_orientation+zero_orientation,2f, paint);*/
 
         
         count++;
--- tests/PathDrawer2D/src/me/pacho/android/PathDrawer2D/PathManager.java
+++ tests/PathDrawer2D/src/me/pacho/android/PathDrawer2D/PathManager.java
@@ -1,3 +1,7 @@
+/*This version of the Path Manager Class uses the orientation matrix provided by the android environment to make
+ *  the path calculations.
+ */
+
 package me.pacho.android.PathDrawer2D;
 
 
@@ -8,47 +12,139 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.util.Log;
 
 public class PathManager implements SensorEventListener{
 	
 	SensorManager mSensorManager;
-	//ArrayList<Float[]> radialVelocities;
-	//ArrayList<Float[]> linealAccelerations;
 	ArrayList<Float[]> trajectory;
-	float[] lastLinealAcceleration={0f,0f,0f};
-	float[] lastLinealVelocity={0f,0f,0f};
-	float[] lastRadialVelocity={0f,0f,0f};
-	long lastTimeStamp=0;
+	
+	
+	public float[] lastLinealAcceleration={0f,0f,0f};
+	public float[] lastLinealVelocity={0f,0f,0f};
+	public float[] lastMagnetic={0f,0f,0f};
+	public float[] lastOrientation ={0f,0f,0f};
+	public float[] lastGravity ={0f,0f,0f};
+	public float[] lastNetAcceleration={0f,0f,0f};
+
+	//float[] lastRadialVelocity={0f,0f,0f};
+
+    
+    private float[] mRotationM = new float[9];               // Use [16] to co-operate with android.opengl.Matrix 
+    private float[] mRemapedRotationM = new float[9]; 
+
+
+    public long lastTimeStamp=0;
+	//deltaTime is in seconds
+    public double deltaTime=0;
+	
+    public long nanoDeltaTime=0;
 	
 	public PathManager(){
 		trajectory=new ArrayList<Float[]>();
+		
+		Float[] temp={0f,0f,0f};
+		trajectory.add(temp);
+		
         mSensorManager = (SensorManager) Drawer.getContext().getSystemService(Context.SENSOR_SERVICE);
+        
         mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_FASTEST);
-        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE), SensorManager.SENSOR_DELAY_FASTEST);
+        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY), SensorManager.SENSOR_DELAY_FASTEST);
+        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD), SensorManager.SENSOR_DELAY_FASTEST);
+
+        //mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE), SensorManager.SENSOR_DELAY_FASTEST);
+
         
 	}
 
 	@Override
 	public void onSensorChanged(SensorEvent event) {
+
+
 		
-        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
+			
+		if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
+    		Log.i("Last Time Stamp",lastTimeStamp+","+event.sensor.getType());
+
         	if(lastTimeStamp!=0){
-        		
+    			nanoDeltaTime=event.timestamp-lastTimeStamp;
+    			deltaTime=(double)(nanoDeltaTime/1000000000d);
+            	calculateOrientation();
+            	calculateNetAcceleration();
+            	correctAcceleration();
+            	//approx 1.
+            	calculateMovement();
+
         	}
     		copyArray(lastLinealAcceleration, event.values);
-    		lastTimeStamp=event.timestamp;
+        	//For the next change
+        	calculateNextLinealVelocity();
+        	lastTimeStamp=event.timestamp;
+
         }
-        else if(event.sensor.getType() ==Sensor.TYPE_GYROSCOPE){
+        
+        else if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD){
+    		copyArray(lastMagnetic, event.values);
+        }
+        
+        else if (event.sensor.getType() == Sensor.TYPE_GRAVITY){
+    		copyArray(lastGravity, event.values);
+        }
+        
+        
+        
+        /*else if(event.sensor.getType() ==Sensor.TYPE_GYROSCOPE){
         	if(lastTimeStamp!=0){
         		
         	}
     		copyArray(lastRadialVelocity, event.values);
     		lastTimeStamp=event.timestamp;
-        }
+        }*/
         
 	}
 	
-	
+	private void calculateMovement() {
+		Float[] lastPosition=trajectory.get(trajectory.size()-1);
+		Float[] newPosition={(float) (lastPosition[0]+lastLinealVelocity[0]*deltaTime),
+							(float) (lastPosition[1]+lastLinealVelocity[1]*deltaTime),
+							(float) (lastPosition[2]+lastLinealVelocity[2]*deltaTime)};
+		trajectory.add(newPosition);
+	}
+
+	private void calculateNextLinealVelocity() {
+		lastLinealVelocity[0]+=lastNetAcceleration[0]*deltaTime;
+		lastLinealVelocity[1]+=lastNetAcceleration[2]*deltaTime;
+		lastLinealVelocity[2]+=lastNetAcceleration[1]*deltaTime;
+	}
+
+	private void correctAcceleration() {
+		//Rotation in X
+		float a=lastOrientation[0];
+		lastNetAcceleration[1]=(float) (lastNetAcceleration[1]*Math.cos(a)-lastNetAcceleration[2]*Math.sin(a));
+		lastNetAcceleration[2]=(float) (lastNetAcceleration[1]*Math.sin(a)+lastNetAcceleration[2]*Math.cos(a));
+		//Rotation in Y
+		a=lastOrientation[1];
+		lastNetAcceleration[0]=(float) (lastNetAcceleration[0]*Math.cos(a)+lastNetAcceleration[2]*Math.sin(a));
+		lastNetAcceleration[2]=(float) (-lastNetAcceleration[0]*Math.sin(a)+lastNetAcceleration[2]*Math.cos(a));
+		//Rotation in Z
+		a=lastOrientation[1];
+		lastNetAcceleration[0]=(float) (lastNetAcceleration[0]*Math.cos(a)-lastNetAcceleration[1]*Math.sin(a));
+		lastNetAcceleration[1]=(float) (lastNetAcceleration[0]*Math.sin(a)+lastNetAcceleration[1]*Math.cos(a));
+		
+	}
+
+	private void calculateNetAcceleration() {
+			float[] resp={lastLinealAcceleration[0]-lastGravity[0],lastLinealAcceleration[1]-lastGravity[1],lastLinealAcceleration[2]-lastGravity[2]};
+			copyArray(lastNetAcceleration,resp);
+	}
+
+	private void calculateOrientation(){
+        if(SensorManager.getRotationMatrix(mRotationM, null, lastLinealAcceleration, lastMagnetic)){
+        	SensorManager.remapCoordinateSystem(mRotationM, SensorManager.AXIS_X, SensorManager.AXIS_Z, mRemapedRotationM); 
+            SensorManager.getOrientation(mRemapedRotationM, lastOrientation); 
+
+        }
+	}
 
 	@Override
 	public void onAccuracyChanged(Sensor arg0, int arg1) {
@@ -58,5 +154,9 @@ public class PathManager implements SensorEventListener{
 
 	private void copyArray(float[] destination, float[] source) {
 		System.arraycopy(source, 0, destination, 0, 3);
+	}
+	
+	public Float[] getLastPosition(){
+		return trajectory.get(trajectory.size()-1);
 	}
 }
